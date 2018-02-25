b785535927f94aeb2d348415bff855c1c0addf1d
monochromegane <dev.kuro.obi@gmail.com>
2018-02-25 22:03:36 +0900

    Fixed broken tests.

--- algorithm_test.go
+++ algorithm_test.go
@@ -2,30 +2,30 @@ package smartsifter
 
 import "testing"
 
-func TestNewCategoricalAlgorism(t *testing.T) {
+func TestNewCategoricalAlgorithm(t *testing.T) {
 	cellNum := 0
-	algo := newCategoricalAlgorism(1.0, 1.0, cellNum)
-	if _, ok := algo.(NoOpCategoricalAlgorism); !ok {
-		t.Errorf("newCategoricalAlgorism should return NoOpCategoricalAlgorism when cellNum is zero.")
+	algo := newCategoricalAlgorithm(1.0, 1.0, cellNum)
+	if _, ok := algo.(NoOpCategoricalAlgorithm); !ok {
+		t.Errorf("newCategoricalAlgorithm should return NoOpCategoricalAlgorithm when cellNum is zero.")
 	}
 
 	cellNum = 1
-	algo = newCategoricalAlgorism(1.0, 1.0, cellNum)
+	algo = newCategoricalAlgorithm(1.0, 1.0, cellNum)
 	if _, ok := algo.(*SDLE); !ok {
-		t.Errorf("newCategoricalAlgorism should return SDLE algorism when cellNum > 0.")
+		t.Errorf("newCategoricalAlgorithm should return SDLE algorism when cellNum > 0.")
 	}
 }
 
-func TestNewContinuousAlgorism(t *testing.T) {
+func TestNewContinuousAlgorithm(t *testing.T) {
 	mixtureNum := 0
-	algo := newContinuousAlgorism(1.0, 1.0, mixtureNum, 2)
-	if _, ok := algo.(NoOpContinuousAlgorism); !ok {
-		t.Errorf("newContinuousAlgorism should return NoOpContinuousAlgorism when mixtureNum is zero.")
+	algo := newContinuousAlgorithm(1.0, 1.0, mixtureNum, 2)
+	if _, ok := algo.(NoOpContinuousAlgorithm); !ok {
+		t.Errorf("newContinuousAlgorithm should return NoOpContinuousAlgorithm when mixtureNum is zero.")
 	}
 
 	mixtureNum = 1
-	algo = newContinuousAlgorism(1.0, 1.0, mixtureNum, 2)
+	algo = newContinuousAlgorithm(1.0, 1.0, mixtureNum, 2)
 	if _, ok := algo.(*SDEM); !ok {
-		t.Errorf("newContinuousAlgorism should return SDEM algorism when mixtureNum > 0.")
+		t.Errorf("newContinuousAlgorithm should return SDEM algorism when mixtureNum > 0.")
 	}
 }
