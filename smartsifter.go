3d54b1ea1d586ffcf59bf29afdd4489acff3cfc2
monochromegane <dev.kuro.obi@gmail.com>
2018-02-25 19:36:47 +0900

    Implemented Input interface.

--- smartsifter.go
+++ smartsifter.go
@@ -1,5 +1,7 @@
 package smartsifter
 
+import "math"
+
 type SmartSifter struct {
 	categoricalAlgorism CategoricalAlgorism
 	continuousAlgorism  ContinuousAlgorism
@@ -10,4 +12,15 @@ func NewSmartSifter(discount, alpha, beta float64, cellNum, mixtureNum, dim int)
 		categoricalAlgorism: newCategoricalAlgorism(discount, beta, cellNum),
 		continuousAlgorism:  newContinuousAlgorism(discount, alpha, mixtureNum, dim),
 	}
+}
+
+func (ss *SmartSifter) Input(X []int, Y []float64, update bool) float64 {
+	return ss.logLoss(
+		ss.categoricalAlgorism.input(X, update),
+		ss.continuousAlgorism.input(Y, update),
+	)
+}
+
+func (ss SmartSifter) logLoss(p, q float64) float64 {
+	return -math.Log(p * q)
 }
